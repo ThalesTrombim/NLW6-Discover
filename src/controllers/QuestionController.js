a893286922775c3cbf00fa4add802b6c493bc77c
Thales Trombim <68087477+ThalesTrombim@users.noreply.github.com>
2021-06-30 17:19:04 -0300

    aula 4

---
+++ src/controllers/QuestionController.js
@@ -0,0 +1,8 @@
+module.exports = {
+    index(req, res) {
+        const roomId = req.params.room;
+        const questionId = req.params.question;
+        const action = req.params.action;
+        const password = req.body.password
+    }
+}
